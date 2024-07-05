389f38bf120f0a7996a3049de2b36ec0b4e07924
Carlos Abarca Vega <soporte@compuelec.cl>
2024-07-05 13:50:20 -0400

    refactor: Remove unused NotificationService class

--- src/modules/messages/messages.module.ts
+++ src/modules/messages/messages.module.ts
@@ -4,18 +4,12 @@ import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
 import { Message } from './entities/message.entity';
 import { UserValidationService } from 'src/services/user-validation.service';
-import { NotificationService } from 'src/services/notification.service';
 import { HttpModule } from '@nestjs/axios';
 import { TimeService } from 'src/services/time.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Message]), HttpModule],
   controllers: [MessagesController],
-  providers: [
-    MessagesService,
-    UserValidationService,
-    NotificationService,
-    TimeService,
-  ],
+  providers: [MessagesService, UserValidationService, TimeService],
 })
 export class MessagesModule {}
